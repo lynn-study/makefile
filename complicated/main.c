1dd0938c67cabef8d5bbd69227b2e86440e2fe18
Lynn <shuai.zhong@sim.com>
2014-04-21 10:09:08 +0800

    Add the files I have writen
    Signed-off-by: Lynn <shuai.zhong@sim.com>

---
+++ complicated/main.c
@@ -0,0 +1,8 @@
+#include "foo.h"
+
+int main(void)
+{
+        foo(5);
+
+        return 0;
+}
---
+++ huge/code/huge/src/main.c
@@ -0,0 +1,10 @@
+#include "foo.h"
+#include "bar.h"
+
+int main(void)
+{
+        bar();
+        foo();
+
+        return 0;
+}
