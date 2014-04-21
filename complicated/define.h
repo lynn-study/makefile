b44ff3b6ca237182b0c2b69f04349dcfee136f0a
Lynn <sotii@163.com>
2014-04-21 10:09:08 +0800

    Add the files I have writen
    Signed-off-by: Lynn <sotii@163.com>

---
+++ complicated/define.h
@@ -0,0 +1,5 @@
+#ifndef __DEFINE_H
+#define __DEFINE_H
+#include "other.h"
+
+#endif
---
+++ complicated/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void foo(int value)
+{
+        printf("%s, value is %d\n", HELLO, value);
+}
